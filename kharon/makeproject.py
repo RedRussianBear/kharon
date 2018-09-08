7246f416ca55df2d6c964599384fd5df69c71aa6
UYasher <shriyash.upadhyay@gmail.com>
2018-09-08 08:28:30 -0400

    Added makeproject functionality

--- kharon/makeproject.py
+++ kharon/makeproject.py
@@ -1,6 +1,11 @@
 import os
 from pkg_resources import resource_filename, Requirement
+from shutil import copyfile
 
-template_files = os.listdir(resource_filename(Requirement.parse("kharon"), "kharon/projecttemplates/"))
+template_path = resource_filename(Requirement.parse("kharon"), "kharon/projecttemplates/")
+template_files = os.listdir(template_path)
 
-print(template_files)
+for filename in template_files:
+    if not filename in ['__init__.py', '__pycache__']:
+        print(template_path+filename)
+        copyfile(template_path+filename, os.getcwd()+"/"+filename)
