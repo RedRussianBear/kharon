e02211fff7d3b4ec6d8716d75bb69190d6e041f2
RedRussianBear <mkhrenov34@gmail.com>
2018-09-07 22:44:58 -0400

    Created package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kharon',
+      version='0.1',
+      description='Simplifying hardware/iot development with a Django style batteries-included framework',
+      url='https://github.com/RedRussianBear/kharon',
+      author='Mikhail Khrenov, Sahil Kochar, Shriyash Upadhyay',
+      author_email='mkhrenov34@gmail.com',
+      license='BSD',
+      packages=['kharon'],
+      zip_safe=False)
