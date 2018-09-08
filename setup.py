c2619cee5679b794eeb701eabf0f825f3d4ae899
RedRussianBear <mkhrenov34@gmail.com>
2018-09-08 07:48:54 -0400

    Actually fixed project templates in PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='kharon',
-      version='0.1.2.3',
+      version='0.1.2.4',
       description='Simplifying hardware/iot development with a Django style batteries-included framework',
       url='https://github.com/RedRussianBear/kharon',
       author='Mikhail Khrenov, Sahil Kochar, Shriyash Upadhyay',
       author_email='mkhrenov34@gmail.com',
       license='BSD',
       packages=['kharon'],
-      package_data={'kharon': ['kharon/*', 'kahron/projecttemplates/*']},
+      package_data={'kharon': ['*', 'projecttemplates/*']},
       zip_safe=False)
