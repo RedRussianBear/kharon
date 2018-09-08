2030f3067a4a945a99bd642a672ae2041e4d7ce4
RedRussianBear <mkhrenov34@gmail.com>
2018-09-08 07:35:03 -0400

    Created symbol table generator and fun_types decorator

--- kharon/ferry.py
+++ kharon/ferry.py
@@ -1,9 +1,49 @@
 import hashlib
+import inspect
+import copy
 
 
-def make_map(device):
+class Symbol:
 
+    def __init__(self, name, symbol_type):
+        self.name = name
+        self.type = symbol_type
+
+
+def get_functions(device):
+    functions = inspect.getmembers(device, lambda a: (inspect.isroutine(a)))
+    functions = [a for a in functions if not (a[0].startswith('__') and a[0].endswith('__'))]
+    return functions
+
+
+def get_members(device):
+    members = inspect.getmembers(device, lambda a: not (inspect.isroutine(a)))
+    members = [a for a in members if not (a[0].startswith('__') and a[0].endswith('__'))]
+    return members
+
+
+def gen_device_symtable(device):
+    table = {}
+
+    for member in get_members(device):
+        table[member[0]] = Symbol(member[0], type(member[1]))
+
+    for func in get_functions(device):
+        table[func[0]] = Symbol(func[0], type(func[1]))
+
+    return table
+
+
+def gen_function_symtable(func, device_symtable):
+    table = copy.copy(device_symtable)
+
+    for parameter, p_type in zip([p[0] for p in inspect.signature(func).parameters], func.types):
+        table[parameter] = Symbol(parameter, p_type)
+
+
+def make_comm_map(device):
     comm_map = {}
+
     for func in device.__dict__.values():
         m = hashlib.md5()
         m.update(func.__name__)
--- kharon/types.py
+++ kharon/types.py
@@ -2,30 +2,41 @@ class Type:
     def to_c_string(self):
         pass
 
+
 class Int(Type):
     def to_c_string(self):
         return "int"
+
 
 class float(Type):
     def to_c_string(self):
         return "float"
 
+
 class Double(Type):
     def to_c_string(self):
         return "double"
+
 
 class Char(Type):
     def to_c_string(self):
         return "char"
 
+
 class Void(Type):
     def to_c_string(self):
         return "void"
+
 
 class Short(Type):
     def to_c_string(self):
         return "short"
 
+
 class Long(Type):
     def to_c_string(self):
         return "long"
+
+
+def fun_types(func, *types):
+    func.types = list(types)
