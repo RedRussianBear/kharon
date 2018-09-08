b3a66e03fdbc359825708696a8883e1c8afadca8
sahil <sahil1topgear@gmail.com>
2018-09-07 23:03:23 -0400

    Preliminary definition of some hardware elements that can be used. Still requires information of exactly how we will convert the hardware elements to C code.

---
+++ kharon/hardware.py
@@ -0,0 +1,26 @@
+#LED, Analog_Pin, Digital_pin, Switch, Pushbutton
+
+class Hardware:
+    def to_c_string(self):
+        pass
+
+
+class Led(Hardware):
+    def to_c_string(self):
+        pass
+
+class AnalogPin(Hardware):
+    def to_string(self):
+        pass
+
+class DigitalPin(Hardware):
+    def to_c_string(self):
+        pass
+
+class Switch(Hardware):
+    def to_c_string(self):
+        pass
+
+class Button(Hardware):
+    def to_c_string(self):
+        pass
