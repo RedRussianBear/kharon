0e4569fabf39733a4059ba3d46cd068856f8c521
RedRussianBear <mkhrenov34@gmail.com>
2018-09-13 12:49:59 -0400

    PyPI description update

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='kharon',
-      version='0.1.5.13',
+      version='0.1.5.14',
       description='Simplifying hardware/iot development with a Django style batteries-included framework',
       url='https://github.com/RedRussianBear/kharon',
       author='Mikhail Khrenov, Sahil Kochar, Shriyash Upadhyay',
       author_email='mkhrenov34@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       license='BSD',
       packages=['kharon'],
       package_data={'kharon': ['*', 'project_templates/*']},
